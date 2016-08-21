8adf95f2727985de5e3ba5c398bfe82a4a16528d
MykolaShevchuk <708029@gmail.com>
2016-08-21 23:31:00 +0300

    Ready for deploy

---
+++ index.js
@@ -0,0 +1,12 @@
+var port = process.env.PORT || 3000;
+var express = require('express');
+var app = express();
+var server = require('http').Server(app);
+
+server.listen(port);
+
+app.use('/app', express.static('app'));
+
+app.get('/', function (req, res) {
+  res.sendFile(__dirname + '/index.html');
+});
